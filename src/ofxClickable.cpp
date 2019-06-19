e22c9d61b2041fa97579d40e600bda905e22ac5e
genekogan <kogan.gene@gmail.com>
2019-06-19 13:57:53 -0400

    rounded corners, gradients, fbo draw, more intricate

--- src/ofxClickable.cpp
+++ src/ofxClickable.cpp
@@ -3,7 +3,10 @@
 
 //--------------------------------------------------------------
 ofxClickable::ofxClickable() {
-    rect = ofRectangle(0, 0, 0, 0);
+    rect = ofRectangle(0, 0, 1, 1);
+    margin = 3;
+    bGradientAmt = 0;
+    roundCorners = 0;
     isHover = false;
     isPressed = false;
     isActive = false;
@@ -11,9 +14,10 @@ ofxClickable::ofxClickable() {
     name = "";
     cHover = ofColor::orange;
     cPressed = ofColor::red;
-    cActive = ofColor::green;
+    cActive = ofColor::lightGreen;
     cString = ofColor::black;
     cBackground = ofColor::white;
+    cActiveBackground = ofColor(220, 245, 220);
 }
 
 //--------------------------------------------------------------
@@ -29,12 +33,26 @@ void ofxClickable::setName(string name) {
 }
 
 //--------------------------------------------------------------
+void ofxClickable::setActive(bool active) {
+    isActive = active;
+    updateFbo();
+}
+
+//--------------------------------------------------------------
 void ofxClickable::loadIcon(string path) {
     this->iconPath = path;
     icon.load(iconPath);
     if (icon.isAllocated()) {
-        icon.resize(rect.getWidth(), rect.getHeight());
+        int iw = icon.getWidth();
+        int ih = icon.getHeight();
+        float aspect = float(iw) / ih;
+        if (aspect > rect.getWidth() / rect.getHeight()) {
+            icon.resize(rect.getWidth(), rect.getWidth()/aspect);
+        } else {
+            icon.resize(aspect * rect.getHeight(), rect.getHeight());
+        }
     }
+    updateFbo();
 }
 
 //--------------------------------------------------------------
@@ -50,38 +68,110 @@ void ofxClickable::resize(int w, int h) {
     if (icon.isAllocated()) {
         icon.resize(rect.getWidth(), rect.getHeight());
     }
+    updateFbo();
+}
+
+//--------------------------------------------------------------
+void ofxClickable::updateFbo() {
+    fboMask.allocate(rect.getWidth(), rect.getHeight());
+    
+    // draw the rounded mask
+    fboMask.begin();
+    ofClear(0, 0);
+    ofSetColor(ofColor::white);
+    ofDrawRectRounded(0, 0, rect.getWidth(), rect.getHeight(), roundCorners);
+    fboMask.end();
+    
+    // draw the FBO
+    fboBg.allocate(rect.getWidth(), rect.getHeight());
+    fboBg.begin();
+    ofClear(0, 0);
+
+    // draw background or gradient
+    if (bGradientAmt > 0) {
+        ofColor color = isActive ? cActiveBackground : cBackground;
+        ofColor color2 = ofColor(ofClamp(color.r-bGradientAmt, 0, 255),
+                                 ofClamp(color.g-bGradientAmt, 0, 255),
+                                 ofClamp(color.b-bGradientAmt, 0, 255));
+        ofBackgroundGradient(color, color2, OF_GRADIENT_LINEAR);
+    }
+    else {
+        ofColor color = isActive ? cActiveBackground : cBackground;
+        ofBackground(color);
+    }
+
+    // draw the icon
+    if (icon.isAllocated()) {
+        ofSetColor(ofColor::white);
+        float x = 0.5 * (rect.getWidth() - icon.getWidth());
+        float y = 0.5 * (rect.getHeight() - icon.getHeight());
+        icon.draw(x, y);
+    }
+    
+    fboBg.end();
+}
+
+//--------------------------------------------------------------
+void ofxClickable::crop(int x, int y, int w, int h) {
+    rect.setWidth(w);
+    rect.setHeight(h);
+    if (icon.isAllocated()) {
+        icon.crop(x, y, w, h);
+    }
+    updateFbo();
 }
 
 //--------------------------------------------------------------
 void ofxClickable::setFromTexture(ofTexture * texture) {
     texture->readToPixels(icon);
+    icon.update();
+    updateFbo();
+}
+
+//--------------------------------------------------------------
+void ofxClickable::setFromPixels(ofPixels * pixels) {
+    icon.setFromPixels(*pixels);
+    updateFbo();
 }
 
 //--------------------------------------------------------------
 void ofxClickable::draw() {
     ofPushStyle();
-    
+
+    // draw outer rectangle
     ofFill();
     if (isActive) {
         ofSetColor(cActive);
-        ofDrawRectangle(rect.getX()-4, rect.getY()-4, rect.getWidth()+8, rect.getHeight()+8);
+        if (roundCorners > 0) {
+            ofDrawRectRounded(rect.getX()-margin, rect.getY()-margin, rect.getWidth()+2*margin, rect.getHeight()+2*margin, roundCorners);
+        } else {
+            ofDrawRectangle(rect.getX()-margin, rect.getY()-margin, rect.getWidth()+2*margin, rect.getHeight()+2*margin);
+        }
     }
     if (isHover) {
         ofSetColor(cHover);
-        ofDrawRectangle(rect.getX()-4, rect.getY()-4, rect.getWidth()+8, rect.getHeight()+8);
+        if (roundCorners > 0) {
+            ofDrawRectRounded(rect.getX()-margin, rect.getY()-margin, rect.getWidth()+2*margin, rect.getHeight()+2*margin, roundCorners);
+        } else {
+            ofDrawRectangle(rect.getX()-margin, rect.getY()-margin, rect.getWidth()+2*margin, rect.getHeight()+2*margin);
+        }
     }
     if (isPressed) {
         ofSetColor(cPressed);
-        ofDrawRectangle(rect.getX()-4, rect.getY()-4, rect.getWidth()+8, rect.getHeight()+8);
+        if (roundCorners > 0) {
+            ofDrawRectRounded(rect.getX()-margin, rect.getY()-margin, rect.getWidth()+2*margin, rect.getHeight()+2*margin, roundCorners);
+        } else {
+            ofDrawRectangle(rect.getX()-margin, rect.getY()-margin, rect.getWidth()+2*margin, rect.getHeight()+2*margin);
+        }
     }
     
-    ofSetColor(cBackground);
-    ofDrawRectangle(rect);
-    if (icon.isAllocated()) {
-        ofSetColor(ofColor::white);
-        icon.draw(rect.getX(), rect.getY());
-    }
-    if (name != "" && font != NULL) {
+    // background gradient
+    ofSetColor(ofColor::white);
+    fboBg.getTexture().setAlphaMask(fboMask.getTexture());
+    fboBg.draw(rect.getX(), rect.getY());
+    
+    // draw string on top of the icon
+    if (name != "" && font != NULL && !icon.isAllocated()) {
         ofSetColor(cString);
         int fw = font->getStringBoundingBox(name, 0, 0).getWidth();
         int fh = font->getStringBoundingBox(name, 0, 0).getHeight();
@@ -94,9 +184,73 @@ void ofxClickable::draw() {
 }
 
 //--------------------------------------------------------------
+void ofxClickable::buttonClicked() {
+    ofNotifyEvent(clickEvent, this);
+}
+
+//--------------------------------------------------------------
 void ofxClickable::setPosition(int x, int y) {
     rect.setX(x);
     rect.setY(y);
+}
+
+//--------------------------------------------------------------
+void ofxClickable::setBackgroundGradient(int bGradientAmt) {
+    this->bGradientAmt = bGradientAmt;
+    updateFbo();
+}
+
+//--------------------------------------------------------------
+void ofxClickable::setCornerRounded(int roundCorners) {
+    this->roundCorners = roundCorners;
+    updateFbo();
+}
+
+//--------------------------------------------------------------
+void ofxClickable::setActiveColor(ofColor active) {
+    cActive=active;
+    updateFbo();
+}
+
+//--------------------------------------------------------------
+void ofxClickable::setHoverColor(ofColor hover) {
+    cHover=hover;
+    updateFbo();
+}
+
+//--------------------------------------------------------------
+void ofxClickable::setPressedColor(ofColor pressed) {
+    cPressed=pressed;
+    updateFbo();
+}
+
+//--------------------------------------------------------------
+void ofxClickable::setBackgroundColor(ofColor cbg) {
+    cBackground=cbg;
+    updateFbo();
+}
+//--------------------------------------------------------------
+void ofxClickable::setActiveBackgroundColor(ofColor cbg) {
+    cActiveBackground=cbg;
+    updateFbo();
+}
+
+//--------------------------------------------------------------
+void ofxClickable::setStringColor(ofColor cstr) {
+    cString=cstr;
+    updateFbo();
+}
+
+//--------------------------------------------------------------
+void ofxClickable::setMargin(int mgn) {
+    margin=mgn;
+    updateFbo();
+}
+
+//--------------------------------------------------------------
+void ofxClickable::setFont(ofTrueTypeFont *font_) {
+    this->font = font_;
+    updateFbo();
 }
 
 //--------------------------------------------------------------
@@ -122,9 +276,4 @@ void ofxClickable::mouseReleased(int x, int y){
         isPressed = false;
         buttonClicked();
     }
-}
-
-//--------------------------------------------------------------
-void ofxClickable::buttonClicked() {
-    ofNotifyEvent(clickEvent, this);
 }
--- src/ofxClickable.h
+++ src/ofxClickable.h
@@ -9,28 +9,39 @@ public:
     
     virtual void setup(string msg, float x, float y, float w, float h);
     virtual void setName(string name);
-    virtual void setFont(ofTrueTypeFont *font_) {this->font = font_;}
+    virtual void setFont(ofTrueTypeFont *font_);
     virtual void loadIcon(string path);
     virtual void saveIcon(string path);
+    virtual void crop(int x, int y, int w, int h);
     virtual void resize(int w, int h);
     virtual void setFromTexture(ofTexture * texture);
+    virtual void setFromPixels(ofPixels * pixels);
     
-    virtual void setActive(bool active) {isActive=active;}
-    virtual void setActiveColor(ofColor active) {cActive=active;}
-    virtual void setHoverColor(ofColor hover) {cHover=hover;}
-    virtual void setPressedColor(ofColor pressed) {cPressed=pressed;}
-    virtual void setBackgroundColor(ofColor cbg) {cBackground=cbg;}
-    virtual void setStringColor(ofColor cstr) {cString=cstr;}
-    
-    string getName() {return name;}
-    bool getActive() {return isActive;}
-    ofColor getActiveColor() {return cActive;}
-    ofColor getHoverColor() {return cHover;}
-    ofColor getPressedColor() {return cPressed;}
-    ofColor getBackgroundColor() {return cBackground;}
-    ofColor getStringColor() {return cString;}
-    ofRectangle getRectangle() {return rect;}
-    
+    virtual void setActive(bool active);
+    virtual void setActiveColor(ofColor active);
+    virtual void setHoverColor(ofColor hover);
+    virtual void setPressedColor(ofColor pressed);
+    virtual void setBackgroundColor(ofColor cbg);
+    virtual void setActiveBackgroundColor(ofColor cbg);
+    virtual void setStringColor(ofColor cstr);
+    virtual void setMargin(int mgn);
+    virtual void setBackgroundGradient(int bGradientAmt);
+    virtual void setCornerRounded(int roundCorners);
+
+    virtual string getName() {return name;}
+    virtual bool getActive() {return isActive;}
+    virtual ofColor getActiveColor() {return cActive;}
+    virtual ofColor getHoverColor() {return cHover;}
+    virtual ofColor getPressedColor() {return cPressed;}
+    virtual ofColor getBackgroundColor() {return cBackground;}
+    virtual ofColor getActiveBackgroundColor() {return cActiveBackground;}
+    virtual ofColor getStringColor() {return cString;}
+    virtual ofRectangle getRectangle() {return rect;}
+    virtual ofImage & getIcon() {return icon;}
+    virtual int getMargin() {return margin;}
+    virtual int getBackgroundGradient() {return bGradientAmt;}
+    virtual int getCornerRounded() {return roundCorners;}
+
     virtual void setPosition(int x, int y);
     virtual void draw();
     
@@ -42,16 +53,24 @@ public:
     virtual void mouseReleased(int x, int y);
     
     ofEvent<void> clickEvent;
-    
+
 protected:
     
+    virtual void updateFbo();
+    
     bool isHover, isPressed, isActive;
-    ofColor cHover, cPressed, cActive, cString, cBackground;
+    ofColor cHover, cPressed, cActive, cString, cBackground, cActiveBackground;
     ofImage icon;
     ofRectangle rect;
+    ofFbo fboBg;
+    ofFbo fboMask;
     string name;
     string iconPath;
     ofTrueTypeFont *font;
+    int margin;
+    int roundCorners;
+    int bGradientAmt;
+
 };
 
 
